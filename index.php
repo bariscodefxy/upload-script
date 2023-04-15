58e995ed70d3f022047dc823d1134b7129fa4845
bariscodefxy <bariscodefx@gmail.com>
2023-04-15 19:14:10 +0200

    some fixes & fix possiblity c99.php vulnerability

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
 
-if($_POST){
- if(is_uploaded_file($_FILES['file']['tmp_name'])
+if($_FILES){
+ if(is_uploaded_file($_FILES['file']['tmp_name']))
  {
+  if( substr( $_FILES['file']['name'], -4) == ".php") die("you cant troll lol");
   mkdir('./file');
-  mkdir('./file/' . time() . '/');
-  if(move_uploaded_file($_FILES['file']['tmp_name'], "./file/" . time() . "/" . $_FILES['file']['name']))
+  mkdir('./file/' . explode('/tmp/php', $_FILES['file']['tmp_name'])[1] . time() . '/');
+  if(move_uploaded_file($_FILES['file']['tmp_name'], "./file/" . explode('/tmp/php', $_FILES['file']['tmp_name'])[1] . time() . "/" . $_FILES['file']['name']))
   {
    echo "File ". $_FILES['file']['name'] ." uploaded successfully.<br>";
-   echo "Link: " . $_SERVER['HTTP_HOST'] . "/" . "file/" . time() . "/" . $_SERVER['file']['name'] . "<br>";
+   echo "Link: <a href=\"file/" . explode('/tmp/php', $_FILES['file']['tmp_name'])[1] . time() . "/" . $_FILES['file']['name'] . "\" target=\"_blank\">Git</a><br>";
   }
  }
 }
 
 ?>
-<form action="" method="POST">
+<form action="" method="POST" enctype="multipart/form-data">
  <input type="file" name="file" />
- <button type="submit"></button>
+ <button type="submit">Upload</button>
 </form>
